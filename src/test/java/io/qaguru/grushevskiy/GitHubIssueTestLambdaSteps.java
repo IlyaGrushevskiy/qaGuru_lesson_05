68c65335ebbe451a54f8dd05d619a78e9eab3eae
Grushevskiy <belpochta05@gmail.com>
2021-02-20 11:54:03 +0300

    Добавлены тесты на поиск ISSUE

---
+++ src/test/java/io/qaguru/grushevskiy/GitHubIssueTestLambdaSteps.java
@@ -0,0 +1,47 @@
+package io.qaguru.grushevskiy;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class GitHubIssueTestLambdaSteps {
+
+    private static final String BASE_URL = "http://github.com";
+    private static final String ISSUES = "Issues";
+    private static final String REPOSITORY = "IlyaGrushevskiy/qaGuru_lesson_05";
+    private static final String ISSUE_NUMBER = "#1";
+
+    @Test
+    @Owner("grushevskiy")
+    @Severity(SeverityLevel.CRITICAL)
+    @Tags({@Tag("web"), @Tag("critical")})
+    @Link(name = "Base URL", value = BASE_URL)
+    @Feature("Issues")
+    @Story("Поск Issue")
+    @DisplayName("Поиск Issue по номеру в репозитории")
+    public void testIssueSearch() {
+
+        step("Открыта главная страница", () -> open(BASE_URL));
+
+        step("Выполнен поиск репозитория", () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+
+        step("Выполнен переход в репозиторий", () -> $(By.linkText(REPOSITORY)).click());
+
+        step("Выполнен переход в ISUUES", () -> $(withText(ISSUES)).click());
+
+        step("Проверено наличие искомого ISSUE", () -> $(withText(ISSUE_NUMBER)).should(Condition.exist));
+    }
+}
---
+++ src/test/java/io/qaguru/grushevskiy/GitHubIssueTestMethodSteps.java
@@ -0,0 +1,33 @@
+package io.qaguru.grushevskiy;
+
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+public class GitHubIssueTestMethodSteps {
+
+    public BaseSteps baseSteps = new BaseSteps();
+
+    private static final String BASE_URL = "http://github.com";
+    private static final String ISSUES = "Issues";
+    private static final String REPOSITORY = "IlyaGrushevskiy/qaGuru_lesson_05";
+    private static final String ISSUE_NUMBER = "#1";
+
+    @Test
+    @Owner("grushevskiy")
+    @Severity(SeverityLevel.CRITICAL)
+    @Tags({@Tag("web"), @Tag("critical")})
+    @Link(name = "Base URL", value = BASE_URL)
+    @Feature("Issues")
+    @Story("Поск Issue")
+    @DisplayName("Поиск Issue по номеру в репозитории")
+    public void issueSearchTest() {
+        baseSteps.openMainPage();
+        baseSteps.searchRepository(REPOSITORY);
+        baseSteps.openRepositoryFromSearch(REPOSITORY);
+        baseSteps.openIssue(ISSUES);
+        baseSteps.checkIssue(ISSUE_NUMBER);
+    }
+}
